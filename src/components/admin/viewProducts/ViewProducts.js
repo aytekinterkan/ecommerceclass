2a184b8c6b3cac092b54ce2edc44b1336df7c2ee
aytekinterkan <aytekinterkan@gmail.com>
2023-08-23 22:58:05 +0300

    23.08.23

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -8,3 +8,55 @@ const ViewProducts = () => {
 }
 
 export default ViewProducts
+
+
+
+// https://firebase.google.com/docs/firestore/query-data/get-data?hl=en&authuser=5#example_data
+
+// https://firebase.google.com/docs/firestore/query-data/order-limit-data?hl=en&authuser=5#order_and_limit_data
+
+
+// https://firebase.google.com/docs/firestore/query-data/listen?hl=en&authuser=5#listen_to_multiple_documents_in_a_collection
+
+// delete için
+// https://firebase.google.com/docs/firestore/manage-data/delete-data?hl=en&authuser=5#delete_documents
+
+
+// https://firebase.google.com/docs/storage/web/delete-files?hl=en&authuser=5#delete_a_file
+
+
+
+// Notiflix.Confirm.show(
+//   'Notiflix Confirm',
+//   'Do you agree with me?',
+//   'Yes',
+//   'No',
+//   function okCb() {
+//     alert('Thank you.');
+//   },
+//   function cancelCb() {
+//     alert('If you say so...');
+//   },
+//   {
+//     width: '320px',
+//     borderRadius: '8px',
+//     // etc...
+//   },
+// );
+
+
+
+// zamaanlama ile ilgili firestone iptali
+
+// https://redux-toolkit.js.org/api/getDefaultMiddleware#customizing-the-included-middleware
+
+
+// setdoc, güncelleme
+// https://firebase.google.com/docs/firestore/manage-data/add-data#set_a_document
+
+
+
+// dosyayı sil
+// https://firebase.google.com/docs/storage/web/delete-files#delete_a_file
+
+

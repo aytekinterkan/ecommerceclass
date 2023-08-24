cb4d675af9d29a34d9fe1a9bc5782148ec15e58f
aytekinterkan <aytekinterkan@gmail.com>
2023-08-24 19:49:41 +0300

    24.08.23

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,16 +1,21 @@
 //// admin panelindeki allproducts componenti
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { collection, deleteDoc, doc, onSnapshot, orderBy, query } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
-import { db } from '../../../firebase/config'
+import { db, storage } from '../../../firebase/config'
 import Loader from '../../loader/Loader'
 import styles from "./ViewProducts.module.scss"
 import { Link } from 'react-router-dom'
 import {FaEdit,FaTrashAlt} from "react-icons/fa"
+import { deleteObject, ref } from 'firebase/storage'
+import Notiflix from 'notiflix'
+import { useDispatch } from 'react-redux'
+import { STORE_PRODUCTS } from '../../../redux/slice/productSlice'
 
 const ViewProducts = () => {
   const [products,setProducts] = useState([])
   const [isLoading,setIsLoading] = useState(false)
+  const dispatch = useDispatch();
 
   useEffect(()=>{
     getProducts()
@@ -29,12 +34,52 @@ const ViewProducts = () => {
         }))
         setProducts(allProducts)
         setIsLoading(false)
+        dispatch(STORE_PRODUCTS({products:allProducts}))
+
     })
   }
     catch (error) {
       setIsLoading(false)
       toast.error(error.message)
     }
+  }
+  const deleteProduct = async(id,imageURL) => {
+    try {
+      await deleteDoc(doc(db,"products",id))
+      const storageRef = ref(storage,imageURL)
+      await deleteObject(storageRef)
+      toast.success("Product deleted successfully")
+    } 
+    catch(error) {
+      toast.error(error.message)
+    }
+  }
+
+  const confirmDelete=(id,imageURL)=>{
+    Notiflix.Confirm.show(
+      'Delete Product!!!',
+      'You are about to delete this product?',
+      'Delete',
+      'Cancel',
+      function okCb() {
+        deleteProduct(id,imageURL);
+      },
+      function cancelCb() {
+        
+      },
+      {
+        width: '320px',
+        borderRadius: '3px',
+        titleColor: 'orangered',
+        okButtonBackground:'orangered',
+        cssAnimationStyle:'zoom'
+        // etc...
+      },
+    );
+
+
+
+
   }
 
 
@@ -81,7 +126,7 @@ const ViewProducts = () => {
                       <FaEdit size={20} color="green"/>
                     </Link>
                     &nbsp;
-                    <FaTrashAlt size={18} color="red"/>
+                    <FaTrashAlt size={18} color="red" onClick={()=> confirmDelete(id,imageURL)}/>
                   </td>
                 </tr>
               )
@@ -135,23 +180,7 @@ export default ViewProducts
 
 
 
-// Notiflix.Confirm.show(
-//   'Notiflix Confirm',
-//   'Do you agree with me?',
-//   'Yes',
-//   'No',
-//   function okCb() {
-//     alert('Thank you.');
-//   },
-//   function cancelCb() {
-//     alert('If you say so...');
-//   },
-//   {
-//     width: '320px',
-//     borderRadius: '8px',
-//     // etc...
-//   },
-// );
+
 
 
 

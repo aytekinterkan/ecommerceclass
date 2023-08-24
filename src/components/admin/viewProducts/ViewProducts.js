1101851d0e50ebf90fc57f11a0466cd2141db9de
aytekinterkan <aytekinterkan@gmail.com>
2023-08-24 12:39:42 +0300

    24.08.23

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,13 +1,122 @@
 //// admin panelindeki allproducts componenti
-import React from 'react'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import { db } from '../../../firebase/config'
+import Loader from '../../loader/Loader'
+import styles from "./ViewProducts.module.scss"
+import { Link } from 'react-router-dom'
+import {FaEdit,FaTrashAlt} from "react-icons/fa"
 
 const ViewProducts = () => {
+  const [products,setProducts] = useState([])
+  const [isLoading,setIsLoading] = useState(false)
+
+  useEffect(()=>{
+    getProducts()
+  },[])
+
+  const getProducts = () => {
+    setIsLoading(true)
+    try {
+      const productsRef = collection(db,"products")
+      const q = query(productsRef, orderBy("createdAt","desc"))
+      onSnapshot(q, (snapshot) => {
+        const allProducts = snapshot.docs.map((doc)=>({
+          id: doc.id,
+          ...doc.data()
+
+        }))
+        setProducts(allProducts)
+        setIsLoading(false)
+    })
+  }
+    catch (error) {
+      setIsLoading(false)
+      toast.error(error.message)
+    }
+  }
+
+
+
   return (
-    <div>ViewProducts</div>
+    <>
+     {isLoading && <Loader/>}
+     <div className={styles.table}>
+     <h2>All Products</h2>
+     {products.length === 0 ? (<p>No product found</p>) : (
+      <table>
+        <thead>
+          <tr>
+              <th>s/n</th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Category</th>
+              <th>Price</th>
+              <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+        {products.map((product,index)=>{
+              const {id,name,price,imageURL,category} = product
+              return(
+                <tr key={id}>
+                  <td>
+                    {index + 1}
+                  </td>
+                  <td>
+                    <img src={imageURL} alt={name} style={{width:"100px"}}/>
+                  </td>
+                  <td>
+                    {name}
+                  </td>
+                  <td>
+                    {category}
+                  </td>
+                  <td>
+                    {`$${price}`}
+                  </td>
+                  <td className={styles.icons}>
+                    <Link to={`/admin/add-product/${id}`}>
+                      <FaEdit size={20} color="green"/>
+                    </Link>
+                    &nbsp;
+                    <FaTrashAlt size={18} color="red"/>
+                  </td>
+                </tr>
+              )
+            })}
+
+
+        </tbody>
+
+
+
+      </table>
+
+     )}
+
+
+
+
+     </div>
+    
+    
+    
+    </>
   )
 }
 
 export default ViewProducts
+
+
+
+
+
+
+
+
+
 
 
 

866c2dc209838b4d49a095b4083e7ca5f9b7bfea
aytekinterkan <aytekinterkan@gmail.com>
2023-08-27 21:19:03 +0300

    27.08.23

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import AdminOnlyRoute from './components/adminOnlyRoute/AdminOnlyRoute'
 import Admin from './pages/admin/Admin'
+import ProductDetails from './components/product/productDetails/ProductDetails'
 
 const App = () => {
   return (
@@ -26,6 +27,7 @@ const App = () => {
       <Route path="/register" element={<Register/>}/>
       <Route path="/reset" element={<Reset/>}/>
       <Route path="/admin/*" element={<AdminOnlyRoute><Admin/></AdminOnlyRoute>}/>
+      <Route path="/product-details/:id" element={<ProductDetails/>}/>
       
     </Routes>
     <Footer/>
--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -9,40 +9,24 @@ import { Link } from 'react-router-dom'
 import {FaEdit,FaTrashAlt} from "react-icons/fa"
 import { deleteObject, ref } from 'firebase/storage'
 import Notiflix from 'notiflix'
-import { useDispatch } from 'react-redux'
-import { STORE_PRODUCTS } from '../../../redux/slice/productSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { STORE_PRODUCTS, selectProducts } from '../../../redux/slice/productSlice'
+import useFetchCollection from '../../../customHooks/useFetchCollection'
 
 const ViewProducts = () => {
-  const [products,setProducts] = useState([])
-  const [isLoading,setIsLoading] = useState(false)
+
+  const {data,isLoading} = useFetchCollection("products")
+  const products = useSelector(selectProducts)
+
   const dispatch = useDispatch();
 
+  
   useEffect(()=>{
-    getProducts()
-  },[])
+    dispatch(STORE_PRODUCTS({products: data}))
+  },[dispatch,data])
 
-  const getProducts = () => {
-    setIsLoading(true)
-    try {
-      const productsRef = collection(db,"products")
-      const q = query(productsRef, orderBy("createdAt","desc"))
-      onSnapshot(q, (snapshot) => {
-        const allProducts = snapshot.docs.map((doc)=>({
-          id: doc.id,
-          ...doc.data()
 
-        }))
-        setProducts(allProducts)
-        setIsLoading(false)
-        dispatch(STORE_PRODUCTS({products:allProducts}))
 
-    })
-  }
-    catch (error) {
-      setIsLoading(false)
-      toast.error(error.message)
-    }
-  }
   const deleteProduct = async(id,imageURL) => {
     try {
       await deleteDoc(doc(db,"products",id))

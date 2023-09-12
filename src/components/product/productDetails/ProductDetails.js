2dab01d5eef8a1d7c160db506885a8cc8bc19271
aytekinterkan <aytekinterkan@gmail.com>
2023-09-12 20:42:34 +0300

    12.09.23

--- src/components/product/productDetails/ProductDetails.js
+++ src/components/product/productDetails/ProductDetails.js
@@ -1,26 +1,36 @@
 //// home sayfasındaki ürünlere tıkladığımızda çıkan product details componenti
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom';
-import useFetchDocument from '../../../customHooks/useFetchDocument';
-import styles from "./ProductDetails.module.scss"
-import spinnerImg from "../../../assets/spinner.gif"
-import { useDispatch, useSelector } from 'react-redux';
-import { ADD_TO_CART, CALCULATE_TOTAL_QUANTITY, DECREASE_CART, selectCartItems } from '../../../redux/slice/cartSlice';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import useFetchDocument from "../../../customHooks/useFetchDocument";
+import styles from "./ProductDetails.module.scss";
+import spinnerImg from "../../../assets/spinner.gif";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  ADD_TO_CART,
+  CALCULATE_TOTAL_QUANTITY,
+  DECREASE_CART,
+  selectCartItems,
+} from "../../../redux/slice/cartSlice";
+import useFetchCollection from "../../../customHooks/useFetchCollection";
+import Card from "../../card/Card";
+import StarsRating from "react-star-rate";
 
 const ProductDetails = () => {
-  const {id} = useParams();
-  const [product,setProduct] = useState(null)
-  const document = useFetchDocument("products",id)
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const document = useFetchDocument("products", id);
+
+  const {data} = useFetchCollection("reviews")
+  const filteredReviews = data.filter((review) => review.productID === id)
 
   const dispatch = useDispatch();
   const cartItems = useSelector(selectCartItems);
+
   const cart = cartItems.find((cart) => cart.id === id);
 
-
-
-  useEffect(()=>{
-    setProduct(document)
-  },[document])
+  useEffect(() => {
+    setProduct(document);
+  }, [document]);
 
   const addToCart = () => {
     dispatch(ADD_TO_CART(product));
@@ -31,8 +41,6 @@ const ProductDetails = () => {
     dispatch(DECREASE_CART(product));
     dispatch(CALCULATE_TOTAL_QUANTITY());
   };
-
-
 
   return (
     <section>
@@ -77,9 +85,34 @@ const ProductDetails = () => {
             </div>
           </>
         )}
+        <Card cardClass={styles.card}>
+          <h3>Product Reviews</h3>
+          <div>
+            {filteredReviews.length === 0 ? (<p>There are no reviews for this product yet</p>) : (
+              <>
+              {filteredReviews.map((item,index)=>{
+                const {rate,review,reviewDate,userName} = item
+                return (
+                  <div key={index} className={styles.review}>
+                    <StarsRating value={rate}></StarsRating>
+                    <p>{review}</p>
+                    <span>
+                      <b>{reviewDate}</b>
+                    </span>
+                    <br/>
+                    <span>
+                      <b>by: {userName}</b>
+                    </span>
+                  </div>
+                )
+              })}
+              </>
+            )}
+          </div>
+        </Card>
       </div>
     </section>
   );
 };
 
-export default ProductDetails
+export default ProductDetails;
